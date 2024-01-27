9856c1738a3978e7dcc240833d516c8da8a1d304
Outvi V <git@outv.im>
2024-01-27 13:57:13 +0800

    chore: collect std/ imports

--- idol.test.ts
+++ idol.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.212.0/assert/mod.ts'
+import { assertEquals } from 'std/assert/mod.ts'
 import { birthdayIdol } from './idol.ts'
 import dayjs from 'dayjs/'
 
